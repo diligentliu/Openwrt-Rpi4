8b817fd3c5bedce01a648ad9cbfb61afb4b48e61
diligentliu <3391569802@qq.com>
2024-09-28 21:09:36 +0800

    fixbug openclash clash_meta download failed

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -13,13 +13,16 @@ mkdir -p files/etc/openclash/core
 
 # replace ${1} with arm64 if it's armv8
 if [ $1 == "armv8" ]; then
-  1="arm64"
+    arch="arm64"
+else
+    arch=$1
 fi
 
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${arch}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
+echo ${CLASH_META_URL}
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
