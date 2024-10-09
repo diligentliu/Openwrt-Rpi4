fd5a5c8f9b348574319d6cd4ec57ab9401951fa7
diligentliu <3391569802@qq.com>
2024-10-09 18:49:36 +0800

    add omz powerlevel10k theme

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -15,6 +15,9 @@ pushd files/root
 # Clone oh-my-zsh repository
 git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
 
+# Clone powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ./.oh-my-zsh/custom/themes/powerlevel10k
+
 # Install extra plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
